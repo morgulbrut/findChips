d1cb1daabee43a5be600dcb53524b24199c069e6
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-10-02 12:56:57 +0200

    refactored to use part

--- main.go
+++ main.go
@@ -5,74 +5,14 @@ import (
 	"os"
 	"strings"
 
-	"github.com/morgulbrut/color"
+	"github.com/morgulbrut/findChips/part"
 	"github.com/morgulbrut/soup"
 )
 
 func main() {
-	part := os.Args[1]
-	getDistributorInfo(part)
-	getPartInfo(part)
-}
-
-func getPartInfo(part string) {
-	url := fmt.Sprintf("https://www.findchips.com/lite/%s", part)
-	resp, _ := soup.Get(url)
-	doc := soup.HTMLParse(resp)
-
-	det := doc.FindAll("a", "class", "sub-header-item")
-	color.Yellow("############## Details ##############")
-	for _, d := range det {
-		if strings.Contains(d.Attrs()["href"], "detail") {
-			url = fmt.Sprintf("https://www.findchips.com%s", d.Attrs()["href"])
-			resp, _ = soup.Get(url)
-			doc = soup.HTMLParse(resp)
-			det := doc.Find("ul", "class", "part-details-list")
-			for _, d := range det.FindAll("li") {
-				parm := d.Find("small").Text()
-				val := strings.TrimSpace(d.Find("p").Text())
-				fmt.Printf("\"%v\",\"%v\"\n", parm, val)
-			}
-			color.Yellow("############## Datasheet Link ##############")
-			ds := doc.Find("div", "class", "datasheet-item").Find("a").Attrs()["href"]
-			fmt.Println(ds)
-			color.Yellow("############## Alternativen ##############")
-			alts := doc.Find("table", "class", "part-suggestions").Find("tbody").FindAll("tr")
-			for _, a := range alts {
-				link := a.Find("td", "class", "td-col-1").Find("a")
-				linkTxt := strings.TrimSpace(link.Text())
-				fmt.Println(linkTxt)
-			}
-		}
-	}
-}
-
-func getDistributorInfo(part string) {
-
-	dists := []string{"Mouser", "Farnel", "Digi-Key", "Avnet", "TTI", "RS"}
-
-	url := fmt.Sprintf("https://www.findchips.com/lite/%s", part)
-	resp, _ := soup.Get(url)
-	doc := soup.HTMLParse(resp)
-
-	res := doc.FindAll("div", "class", "distributor-results")
-	for _, r := range res {
-		distributor := r.Find("h3", "class", "distributor-title").FullText()
-		distributor = strings.TrimSpace(distributor)
-		distributor = strings.Split(distributor, "\n")[0]
-
-		if contains(distributor, dists) {
-
-			distPartNos := r.FindAll("tr", "class", "row")
-			color.Yellow("############## %s ##############", distributor)
-			for _, p := range distPartNos {
-				distributorPartNo := p.Attrs()["data-distino"]
-				partNo := p.Attrs()["data-mfrpartnumber"]
-				desc := p.Children()[5].Find("span", "class", "td-description").Text()
-				fmt.Printf("\"%v\",\"%v\",\"%v\",\"%v\"\n", distributor, partNo, distributorPartNo, desc)
-			}
-		}
-	}
+	prt := os.Args[1]
+	p := getPartInfo(prt)
+	part.PrintPart(p)
 }
 
 func contains(input string, words []string) bool {
@@ -82,4 +22,60 @@ func contains(input string, words []string) bool {
 		}
 	}
 	return false
+}
+
+func getPartInfo(prt string) part.Part {
+
+	var p part.Part
+	p.Partnumber = prt
+
+	url := fmt.Sprintf("https://www.findchips.com/lite/%s", prt)
+	resp, _ := soup.Get(url)
+	doc := soup.HTMLParse(resp)
+
+	dists := []string{"Mouser", "Farnel", "Digi-Key", "Avnet", "TTI", "RS"}
+
+	res := doc.FindAll("div", "class", "distributor-results")
+	for _, r := range res {
+		distributor := r.Find("h3", "class", "distributor-title").FullText()
+		distributor = strings.TrimSpace(distributor)
+		distributor = strings.Split(distributor, "\n")[0]
+		if contains(distributor, dists) {
+			var dist part.Distributor
+			dist.Name = distributor
+			distPartNos := r.FindAll("tr", "class", "row")
+			for _, dpn := range distPartNos {
+				var pn part.Partnumber
+				pn.DistPartnumber = dpn.Attrs()["data-distino"]
+				pn.ManPartnumber = dpn.Attrs()["data-mfrpartnumber"]
+				pn.Desc = dpn.Children()[5].Find("span", "class", "td-description").Text()
+				dist.Partnumbers = append(dist.Partnumbers, pn)
+			}
+			p.Distributors = append(p.Distributors, dist)
+		}
+	}
+
+	det := doc.FindAll("a", "class", "sub-header-item")
+	for _, d := range det {
+		if strings.Contains(d.Attrs()["href"], "detail") {
+			url = fmt.Sprintf("https://www.findchips.com%s", d.Attrs()["href"])
+			resp, _ = soup.Get(url)
+			doc = soup.HTMLParse(resp)
+			det := doc.Find("ul", "class", "part-details-list")
+			for _, d := range det.FindAll("li") {
+				var par part.Parameter
+				par.Param = strings.Trim(d.Find("small").Text(), ":")
+				par.Val = strings.TrimSpace(d.Find("p").Text())
+				p.Parameters = append(p.Parameters, par)
+			}
+			ds := doc.Find("div", "class", "datasheet-item").Find("a").Attrs()["href"]
+			p.Datasheets = append(p.Datasheets, ds)
+			alts := doc.Find("table", "class", "part-suggestions").Find("tbody").FindAll("tr")
+			for _, a := range alts {
+				link := a.Find("td", "class", "td-col-1").Find("a")
+				p.Alternatives = append(p.Alternatives, strings.TrimSpace(link.Text()))
+			}
+		}
+	}
+	return p
 }
---
+++ part/part.go
@@ -0,0 +1,55 @@
+package part
+
+import (
+	"github.com/morgulbrut/color"
+)
+
+type Parameter struct {
+	Param string
+	Val   string
+}
+
+type Partnumber struct {
+	ManPartnumber  string
+	DistPartnumber string
+	Desc           string
+}
+
+type Distributor struct {
+	Name        string
+	Partnumbers []Partnumber
+}
+
+type Part struct {
+	Partnumber   string
+	Parameters   []Parameter
+	Datasheets   []string
+	Alternatives []string
+	Distributors []Distributor
+}
+
+func PrintPart(p Part) {
+	color.Red(p.Partnumber)
+	for _, d := range p.Datasheets {
+		color.Cyan(d)
+	}
+	color.Yellow("Details")
+	for _, p := range p.Parameters {
+		color.Cyan("%s : %s", p.Param, p.Val)
+	}
+	color.Yellow("Alternatives")
+	for _, a := range p.Alternatives {
+		color.Cyan(a)
+	}
+	color.Yellow("Datasheets")
+	for _, a := range p.Datasheets {
+		color.Cyan(a)
+	}
+	color.Yellow("Distributors")
+	for _, d := range p.Distributors {
+		color.Green(d.Name)
+		for _, pn := range d.Partnumbers {
+			color.Cyan("%s : %s : %s\n", pn.ManPartnumber, pn.DistPartnumber, pn.Desc)
+		}
+	}
+}
