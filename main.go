c3d14446ab6914d86eb0410a4aa94ae45747e630
Tillo Bosshart <tillo.bosshart@gmail.com>
2020-01-09 14:51:29 +0100

    WIP

---
+++ main.go
@@ -0,0 +1,12 @@
+// Copyright © 2019 morgulbrut
+// This work is free. You can redistribute it and/or modify it under the
+// terms of the Do What The Fuck You Want To Public License, Version 2,
+// as published by Sam Hocevar. See the LICENSE file for more details.
+
+package main
+
+import "github.com/morgulbrut/findChips/cmd"
+
+func main() {
+	cmd.Execute()
+}
